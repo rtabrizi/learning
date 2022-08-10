97c95609cdd3f7cbb53c4f79a850eacade5304da
Ryan Tabrizi <rtabrizi@berkeley.edu>
2022-08-09 21:23:36 -0700

    got rid of unnecessary block in constructor

--- aladdin/resnet.py
+++ aladdin/resnet.py
@@ -11,10 +11,10 @@ import torchvision.transforms as transforms # transformations on dataset
 import torch
 import torch.nn as nn
 
-class block(nn.Module):
+class Block(nn.Module):
     # identity downsample for moments where there's a dotted line when looking at paper's graph
     def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
-        super(block,self).__init__()
+        super().__init__()
         # block 1 example: input channels = 64, output channels = 64 (we ignore the 256 and instead explicilty write that as out_channels * 4)
         # figure 5 of original paper
         self.expansion = 4
@@ -27,11 +27,15 @@ class block(nn.Module):
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
         self.stride = stride
-        if stride > 1 or in_channels != out_channels * self.expansion:
+
+        # for the first layer, we define in_channels to 64, and out_channels to 64
+        # when we go to make each layer, every layer but the first has an inputted stride of 2 --> skip connection
+        if stride != 1 or in_channels != out_channels * self.expansion:
             # 1x1 convolution
             # we use inputted stride to match feature mapping dimensions
             self.identity_downsample = nn.Sequential(nn.Conv2d(in_channels, out_channels * self.expansion, kernel_size=1, stride=stride),
             nn.BatchNorm2d(out_channels * self.expansion))
+            
     def forward(self, x):
         identity = x.clone()
         x = self.conv1(x)
@@ -42,9 +46,6 @@ class block(nn.Module):
         x = self.relu(x)
         x = self.conv3(x)
         x = self.bn3(x)
-        #according to paper, downsamples are performed with a stride of 2
-        #skip connection if number of channels change or stride isn't 1
-        
         
         if self.identity_downsample is not None:
             identity = self.identity_downsample(identity)
@@ -56,8 +57,8 @@ class block(nn.Module):
 
 class ResNet(nn.Module):
     # layers will be list of how many times per block [3, 4, 6, 3]
-    def __init__(self, block, layers, image_channels, num_classes):
-        super(ResNet, self).__init__()
+    def __init__(self, layers, image_channels, num_classes):
+        super().__init__()
         self.in_channels = 64
 
         #initial layers
@@ -71,10 +72,10 @@ class ResNet(nn.Module):
 
         #for ResNet 50, input_channels = 64, out_channels is really the 1x1, 64 seen in architecture table
         # we still consider out_channels to be 64 even though last layer has 256 channels (for block 1)
-        self.layer1 = self.__make_layer(block, layers[0], out_channels=64, stride=1)
-        self.layer2 = self.__make_layer(block, layers[1], out_channels=128, stride=2)
-        self.layer3 = self.__make_layer(block, layers[2], out_channels=256, stride=2)
-        self.layer4 = self.__make_layer(block, layers[3], out_channels=512, stride=2)
+        self.layer1 = self.__make_layer(layers[0], out_channels=64, stride=1)
+        self.layer2 = self.__make_layer(layers[1], out_channels=128, stride=2)
+        self.layer3 = self.__make_layer(layers[2], out_channels=256, stride=2)
+        self.layer4 = self.__make_layer(layers[3], out_channels=512, stride=2)
 
         #adaptive --> we define the output size of (1,1)
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
@@ -98,28 +99,28 @@ class ResNet(nn.Module):
 
         return x
 
-    def __make_layer(self, block, num_residual_blocks, out_channels, stride):
+    def __make_layer(self, num_residual_blocks, out_channels, stride):
         identity_downsample = None
         layers = []
 
         
 
         #changes number of channels only in first block
-        layers.append(block(self.in_channels, out_channels, identity_downsample, stride))
+        layers.append(Block(self.in_channels, out_channels, identity_downsample, stride))
 
         #figure 5 of paper: now that we're past the downsample, the input_channels = 64 * 4 = 256
         self.in_channels = out_channels*4
 
         for i in range(num_residual_blocks-1):
-            layers.append(block(self.in_channels, out_channels))
+            layers.append(Block(self.in_channels, out_channels))
 
         return nn.Sequential(*layers)
     
 def ResNet50(img_channels=3, num_classes=1000):
-    return ResNet(block, [3,4,6,3], img_channels, num_classes)
+    return ResNet([3,4,6,3], img_channels, num_classes)
 
 def ResNet101(img_channels=3, num_classes=1000):
-    return ResNet(block, [3,8,36,3], img_channels, num_classes)
+    return ResNet([3,8,36,3], img_channels, num_classes)
 
 model = ResNet50(img_channels=1, num_classes=10) # each image is 784 values, 10 possible digits
 
